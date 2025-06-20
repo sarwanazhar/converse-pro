4188b1b3d3640bad510479821cc68fefb4b93f66
tomas0502 <uigraslasu@gmail.com>
2025-06-20 17:36:13 +0530

    json token

---
+++ server/lib/utils.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+//Function to generate a JWT token
+
+export const generateToken = (userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET); 
+    return token;
+}
